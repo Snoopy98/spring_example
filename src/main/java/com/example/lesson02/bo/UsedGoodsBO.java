2a61a407cc6763b272b6be86788db6c8fca72ff7
Test <snoopy980306>
2023-06-15 18:41:18 +0900

    6-15

--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -2,17 +2,22 @@ package com.example.lesson02.bo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.lesson02.dao.UsedGoodsMapper;
 import com.example.lesson02.domain.UsedGoods;
 
 @Service // Spring bean
 public class UsedGoodsBO {
+	//field
+	@Autowired   // Dependency Inject (DI) -> 의존성 주입 => Spring bean 
+	private UsedGoodsMapper usedGoodsMapper;
 	
 	// input: X   
 	// output: List<UsedGoods>  Controller 한테
 	public List<UsedGoods> getUsedGoodsList(){
-		List<UsedGoods> usedGoodsList = ;
+		List<UsedGoods> usedGoodsList = usedGoodsMapper.selectUsedGoodsList();
 		return usedGoodsList;
 	}
 }
